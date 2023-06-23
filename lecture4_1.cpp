d22eda67bcfa325aff7a3253ad1f7e2679421f0f
Ramineni Prithvi <95245461+prithvinani@users.noreply.github.com>
2023-06-23 13:53:55 +0530

    Add files via upload

---
+++ lecture4_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int a,b;
+    // cout<<"enter 2 numbers: ";
+    // cin>>a;
+    // cin>>b;
+    // cout<<(a>b);
+    int a,b,c,d;
+    cout<<"enter 4 numbers";
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+    cout<<(a>b || c>d);
+    return 0;
+}
